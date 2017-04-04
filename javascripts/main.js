e9d76fe95c0cd74a6ca25390db3b5f63a422265e
bryanhmiller <bryankalos@hotmail.com>
2017-04-03 20:12:29 -0500

    Finally got the JSON file to load, errant semicolon had broken the code.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,17 @@
-console.log("I am here");
+function carParse() {
+	console.log("loaded");
+	var data = JSON.parse(this.responseText);
+	console.log("data", data)
+}
+
+function carFail(){
+	console.log("boooo");
+}
+
+var carInventoryRequest = new XMLHttpRequest();
+carInventoryRequest.addEventListener("load", carParse);
+carInventoryRequest.addEventListener("error", carFail);
+carInventoryRequest.open("GET", "javascripts/inventory.json");
+carInventoryRequest.send()
+
+
